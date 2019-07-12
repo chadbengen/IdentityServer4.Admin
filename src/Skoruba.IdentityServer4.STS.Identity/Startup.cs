ff93196aebac0cdfe9ea0488458caf4e5398e3d5
chadbengen <chad@visitingangelswa.com>
2019-07-12 08:40:38 -0700

    Fork/multitenant audit (#10)
    implement auditing

--- src/Skoruba.IdentityServer4.STS.Identity/Startup.cs
+++ src/Skoruba.IdentityServer4.STS.Identity/Startup.cs
@@ -8,6 +8,9 @@ using Skoruba.IdentityServer4.Admin.EntityFramework.Shared.Entities.Identity;
 using Skoruba.IdentityServer4.STS.Identity.Helpers;
 using System.Reflection;
 using Skoruba.IdentityServer4.STS.Identity.DependencyInjection;
+using Skoruba.IdentityServer4.Audit.Sink.DependencyInjection;
+using Skoruba.IdentityServer4.Audit.EntityFramework.DependencyInjection;
+using Skoruba.IdentityServer4.STS.Identity.Configuration.Constants;
 
 namespace Skoruba.IdentityServer4.STS.Identity
 {
@@ -16,6 +19,7 @@ namespace Skoruba.IdentityServer4.STS.Identity
         public IConfiguration Configuration { get; }
         public IHostingEnvironment Environment { get; }
         public ILogger Logger { get; set; }
+        private readonly ILoggerFactory _loggerFactory;
 
         public Startup(IHostingEnvironment environment, ILoggerFactory loggerFactory)
         {
@@ -32,6 +36,7 @@ namespace Skoruba.IdentityServer4.STS.Identity
 
             Configuration = builder.Build();
             Environment = environment;
+            _loggerFactory = loggerFactory;
             Logger = loggerFactory.CreateLogger<Startup>();
         }
 
@@ -64,6 +69,11 @@ namespace Skoruba.IdentityServer4.STS.Identity
 
             // Add services for authentication, including Identity model, IdentityServer4 and external providers
             services.AddAuthenticationServices(Configuration);
+
+            services.AddIdentityServer4Auditing()
+                .AddConsoleSink()
+                .AddSerilogSinkWithDbContext(Configuration.GetConnectionString(ConfigurationConsts.IdentityDbConnectionStringKey), Environment.EnvironmentName)
+                .AddDefaultIdentityServer4Sink();
 
             // Add authorization policies for MVC
             services.AddAuthorizationPolicies();
